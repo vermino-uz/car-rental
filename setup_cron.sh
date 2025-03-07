0b3bd55d1462d9bc5677fc3297bd339e7eb26701
Shukurullayev Elbek <118737003+vermino-uz@users.noreply.github.com>
2025-03-07 15:40:50 +0500

    Update setup_cron.sh

--- setup_cron.sh
+++ setup_cron.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Add cron job to run every 5 minutes
-(crontab -l 2>/dev/null; echo "*/5 * * * * php /www/wwwroot/vermino.uz/bots/orders/ibo/check_rentals.php") | crontab - 
+(crontab -l 2>/dev/null; echo "*/5 * * * * php /www/wwwroot/path/") | crontab - 
